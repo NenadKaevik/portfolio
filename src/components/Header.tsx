fb66ba1d2b5d860b4b8918429a5e5fdfe0827369
Nenad Kaevik <nenadkaevik@gmail.com>
2025-09-22 23:36:15 +0200

    Update photography link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -66,7 +66,7 @@ export const Header = () => {
             <HeaderItem
               active={pathname === "/photography"}
               name="Photography"
-              href="https://unsplash.com/@nenadkaevik"
+              href="https://photo.nenadkaevik.com"
             />
           </ul>
         </div>
